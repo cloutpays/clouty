9f1f031aa9b0646c4908b1a00d738961de5d19e3
Ebrima <ebrima.jobe92@gmail.com>
2019-11-27 13:48:06 -0500

    serverless game function update + confirm text added

--- server/helpers/game.js
+++ server/helpers/game.js
@@ -3,23 +3,35 @@ const connect = require('./db');
 const { parse } = require('url');
 const ObjectId = require('mongodb').ObjectId;
 const cors = require('micro-cors')();
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
-const wrapAsync = handler => (req, res) =>
+const wrapAsync = (handler) => (req, res) =>
   handler(req)
-    .then(result => {
-      res.setHeader(
-        'cache-control',
-        's-maxage=1 maxage=0, stale-while-revalidate',
-      );
+    .then((result) => {
+      res.setHeader('cache-control', 's-maxage=1 maxage=0, stale-while-revalidate');
       return res.json(result);
     })
-    .catch(error => res.status(500).json({ error: error.message }));
+    .catch((error) => res.status(500).json({ error: error.message }));
 
 const gameApi = wrapAsync(async function(req) {
   const data = await json(req);
   console.log(data);
   // Set caching headers to serve stale content (if over a second old)
   // while revalidating fresh content in the background
+  client.messages
+    .create({
+      body: `Thank you for signing up to Clouty! Here is the final step to secure your bet https://cash.app/$Cloutyio/${data.wager}`,
+      from: '+15103302064',
+      to: data.phoneNumber,
+    })
+    .then((message) => {
+      console.log(message.sid);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
   const database = await connect();
   const collection = await database.collection('cloutpays');
