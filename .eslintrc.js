a9272e91249328ae8165a5f173c4b1a8fff302ee
Julius Buckley <julius@goodtime.io>
2019-11-28 03:27:52 -0800

    update linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,20 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'babel'],
-  rules: {},
+  plugins: ['react', 'babel', 'sort-imports-es6-autofix'],
+  rules: {
+    'sort-imports-es6-autofix/sort-imports-es6': [
+      2,
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
