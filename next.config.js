788c28983b504e0da48990aca171f22558aad89a
Julius Buckley <julius@goodtime.io>
2019-11-27 19:52:41 -0800

    update and create config files

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 // next.config.js
 const withCSS = require('@zeit/next-css');
+const withTypescript = require('@zeit/next-typescript');
 require('dotenv').config();
 
-module.exports = withCSS({
-  target: 'serverless',
-  env: {
-    // Reference a variable that was defined in the .env file and make it available at Build Time
-    MONGO_URL: process.env.MONGO_URL,
-  },
-});
+module.exports = withTypescript(
+  withCSS({
+    target: 'serverless',
+    env: {
+      // Reference a variable that was defined in the .env file and make it available at Build Time
+      MONGO_URL: process.env.MONGO_URL,
+    },
+  }),
+);
