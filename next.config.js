d540440fce689e9ad01e832760db737523d357ba
Ebrima <ebrima.jobe92@gmail.com>
2020-02-02 14:56:38 -0800

    add firebase dev configs

--- next.config.js
+++ next.config.js
@@ -8,7 +8,9 @@ module.exports = withCSS({
     // Reference a variable that was defined in the .env file and make it available at Build Time
     MONGO: process.env.MONGO,
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_API_KEY_DEV: process.env.FIREBASE_API_KEY_DEV,
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_APP_ID_DEV: process.env.FIREBASE_APP_ID_DEV,
     STRIPE_CLIENT_DEV: process.env.STRIPE_CLIENT_DEV,
     STRIPE_SECRET_DEV: process.env.STRIPE_SECRET_DEV,
   },
