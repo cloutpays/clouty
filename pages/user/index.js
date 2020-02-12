b3cd766df5d9318b289997e6447e376cabee66e6
ebrima <ebrima.jobe92@gmail.com>
2020-02-11 22:23:59 -0500

    restyled list of submitted games on user dash

--- pages/user/index.js
+++ pages/user/index.js
@@ -21,79 +21,71 @@ const Terms = ({ submissions, user }) => {
           <section className='flex-m flex-l nl3-m nr3-m nl3-l nr3-l'>
             <article className='w-100 w-75-m w-75-l ph3-m ph3-l'>
               <div className='flex-m flex-l flex-wrap items-center justify-between nl3 nr3 pt1 mb4'>
-                <h2 className='ttu mb2 f6 fw5 silver'>Submissions</h2>
-                {submissions.length > 0 ? (
-                  submissions
-                    .map((game) => {
-                      const gameButtonText = !game.question.answer
-                        ? 'Pending'
-                        : game.question.answer === game.answer
-                        ? 'Won'
-                        : 'Lost';
-                      const activeLink = `/games/${game.question.slug}`;
-                      const cardClass = `white br2 shadow-4 pa3 pa4-ns h-100 grow grammy`;
-                      return (
-                        <div
-                          key={`work-${game.question.slug}`}
-                          className='pv2 pa2-ns w-100 w-100-ns'>
-                          <Link href={activeLink}>
-                            <a href={activeLink} className='no-underline white'>
-                              <div className={cardClass}>
-                                <h1 className='f4 mt0 fw7'>
-                                  <span
-                                    role='img'
-                                    aria-label={game.question.emoji_name}>
-                                    {game.question.emoji}
-                                  </span>{' '}
-                                  {`${
-                                    game.question.gameType === 'game'
-                                      ? `Game #${game.question.question}`
-                                      : game.question.question
-                                  }`}{' '}
-                                </h1>
-                                {/* {grammy
-                                  ? grammyRender(game.question)
-                                  : game.question.description} */}
-
-                                <p>{`Wager: $${game.wager}`}</p>
-                                <p>{`Submission: ${game.answer}`}</p>
-                                <span className='bg-white-30 pv1 ph2 f7 f6-ns br-pill b'>
-                                  {gameButtonText}
-                                  <span className='pl1 sans-serif'>→</span>
-                                </span>
-                              </div>
-                            </a>
-                          </Link>
-                        </div>
-                      );
-                    })
-                    .reverse()
-                ) : (
-                  <div className='divide tc relative'>
-                    <h5 className='fw4 ttu mv0 dib bg-white ph3'>
-                      NO GAMES YET
-                    </h5>
-                    <br />
-                    <a
-                      href='/games'
-                      className=' f6 no-underline fw5 mt3 br2 ph3 pv2 dib ba b--blue blue bg-white hover-bg-blue hover-white'>
-                      Start a Game
-                    </a>
-                  </div>
-                )}
-              </div>
-              <hr className='o-20 mt4' />
-              <div className='flex flex-wrap pt3 nl3 nr3'>
                 <div className='w-100 w-50-l ph3 mb3 mb0-l'>
                   <div className='bt bl br b--black-10 br2'>
                     <div className='pa3 bb b--black-10'>
-                      <h4 className='mv0'>Countries</h4>
+                      <h4 className='mv0'>Submissions</h4>
                     </div>
+                    <ul className='list pl0 mt0 measure center'>
+                      {submissions.length > 0 &&
+                        submissions
+                          .map((game) => {
+                            const gameButtonText = !game.question.answer
+                              ? 'Pending'
+                              : game.question.answer === game.answer
+                              ? 'W'
+                              : 'L';
+                            const activeLink = `/games/${game.question.slug}`;
+                            return (
+                              <Link key={activeLink} href={activeLink}>
+                                <li className='flex items-center lh-copy pa3 ph0-l bb b--black-10'>
+                                  <span
+                                    className='w2 h2 w3-ns h3-ns br-100'
+                                    role='img'
+                                    aria-label={game.question.emoji_name}>
+                                    {game.question.emoji}
+                                  </span>
+                                  <div className='pl3 flex-auto'>
+                                    <span className='f6 db black-70'>
+                                      {`${
+                                        game.question.gameType === 'game'
+                                          ? `Game #${game.question.question}`
+                                          : game.question.question
+                                      }`}
+                                    </span>
+                                    <span className='f6 db black-70'>
+                                      {`Submitted Bet: ${game.answer}`}{' '}
+                                    </span>
+                                  </div>
+                                  <div>
+                                    {gameButtonText === 'W' && (
+                                      <div className='bg-green ph1 fw8 f6 white'>
+                                        W
+                                      </div>
+                                    )}
+                                    {gameButtonText === 'L' && (
+                                      <div className='bg-red ph1 fw8 f6 white'>
+                                        L
+                                      </div>
+                                    )}
+                                    {gameButtonText === 'Pending' && (
+                                      <div className='bg-gold ph1 fw8 f6 white'>
+                                        P
+                                      </div>
+                                    )}
+                                    {/* <div className='f6'>+$5</div> */}
+                                  </div>
+                                </li>
+                              </Link>
+                            );
+                          })
+                          .reverse()}
+                    </ul>
                   </div>
                   <a
-                    href='#'
+                    href='/games'
                     className='no-underline fw5 mt3 br2 ph3 pv2 dib ba b--blue blue bg-white hover-bg-blue hover-white'>
-                    All Countries
+                    Play A Game
                   </a>
                 </div>
                 <div className='w-100 w-50-l ph3 mb3 mb0-l'>
