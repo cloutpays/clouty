6345e1ae6bb729a25ac2524b90e43e46254f6b8a
ebrima <ebrima.jobe92@gmail.com>
2020-02-12 16:27:28 -0500

    style the next button for adding to balance

--- components/CardSection.js
+++ components/CardSection.js
@@ -56,7 +56,19 @@ const Checkout = (props) => {
         </a>
       </div>
       <div className='near-black pt3 fw7 pb3'>Total: ${amount}</div>
-      <button onClick={goToCheckout}>Continue</button>
+      <div
+        onClick={goToCheckout}
+        className='f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box'>
+        <span className='pr1'>Next</span>
+        <svg
+          className='w1'
+          data-icon='chevronRight'
+          viewBox='0 0 32 32'
+          style='fill:currentcolor'>
+          <title>chevronRight icon</title>
+          <path d='M12 1 L26 16 L12 31 L8 27 L18 16 L8 5 z'></path>
+        </svg>
+      </div>
     </main>
   );
 };
