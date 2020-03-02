d4255b2a19467c19acadc9896688a46780dfc48c
ebrima <ebrima.jobe92@gmail.com>
2020-03-02 01:45:29 -0800

    terms of service

--- pages/terms.js
+++ pages/terms.js
@@ -3,17 +3,176 @@ import Wrapper from '../components/Wrapper';
 
 const data = {
   description: 'Make money while putting your intuition on the line.',
-  header: `The 🌎's first music betting platform.`,
+  header: `Terms of Service`,
 };
 const Terms = () => (
   <div>
     <Wrapper data={data} className='measure-wide'>
-      <p>
-        <strong>Disclaimer: </strong>Gambling is legal under U.S. federal law.
+      <p className='f5 b'>USER AGREEMENT</p>
+      <p className='f5'>
+        Clouty owns and operates the Website that links to these Terms of Use.
+        We are pleased to offer you access to our Website and the ability to
+        participate in our fantasy sports contests of skill, other content,
+        products, services, and promotions (collectively the
+        &quot;Services&quot;) that we may provide from our Website, subject to
+        these Terms of Use (the &quot;Terms of Use&quot;, or “Terms”), our
+        privacy policy (the &quot;Privacy Policy&quot;) and the Official Rules
+        and Regulations for the applicable contests and promotions (the
+        &quot;Rules&quot; or &quot;Rules and Scoring,&quot; and together with
+        the Terms of Use and the Privacy Policy, the &quot;Agreements&quot;).
       </p>
-      <p>
-        Gambling should be entertaining. Remember that you always risk losing
-        the money you bet, so do not spend more than you can afford to lose.{' '}
+      <p className='f5 b'>CONSIDERATION</p>
+      <p className='f5'>
+        You agree to these Terms of Use by accessing or using the Website,
+        registering for Services offered on the Website, or by accepting,
+        uploading, submitting or downloading any information or content from or
+        to the Website. IF YOU DO NOT AGREE TO BE BOUND BY ALL OF THESE TERMS OF
+        USE, DO NOT USE THE WEBSITE. These Terms of Use constitute a legal
+        agreement between you and Clouty, and shall apply to your use of the
+        Website and the Services even after termination.
+      </p>
+      <p className='f5 b'>CONTEST ENTRY</p>
+      <p className='f5'>
+        Users will be able to visit the Website and view the games available for
+        entry (the &quot;Contests&quot;). Each individual Contest that is not
+        free to enter has an entry fee listed in US dollars. When you select to
+        participate in a Contest and complete the entry process, the listed
+        amount of US dollars will be debited from your Clouty account.
+      </p>
+      <p className='f5 b'>CONTEST OF SKILL</p>
+      <p className='f5'>
+        Contests offered on the Website are contests of skill. Winners are
+        determined by the objective criteria described in the Contest deadline,
+        roster, Rules, scoring, and any other applicable documentation
+        associated with the Contest. From all entries received for each Contest,
+        winners are determined by the individuals who use their skill and
+        knowledge of relevant sports information and fantasy sports rules to
+        accumulate the most points according to the corresponding scoring rules.
+        The Website and Contests may not be used for any form of illicit
+        gambling.
+      </p>
+      <p className='f5 b'>CONTEST STATISTICS AND LIVE SCORING</p>
+      <p className='f5'>
+        To the extent that we offer &apos;live&apos; statistics during gameplay,
+        all &apos;live&apos; statistics and other information provided through
+        the Clouty website and related information sources are unofficial. Live
+        sports statistics and their respective components are offered for
+        informational and/or entertainment purposes only and are not used to
+        determine the results of our Contests. While Clouty and the third
+        parties used to provide the Clouty Services use reasonable efforts to
+        include accurate and up-to-date information, neither Clouty nor its
+        third party providers warrant or make any representations of any kind
+        with respect to the information provided through the Clouty website and
+        related information sources. Clouty and its third party providers shall
+        not be responsible or liable for the accuracy, usefulness, or
+        availability of any information transmitted or made available via the
+        Clouty Website and related information sources, and shall not be
+        responsible or liable for any error or omissions in that information.
+      </p>
+      <p className='f5 b'>PRIZES</p>
+      <p className='f5'>
+        At the conclusion of each Contest, prizes will be awarded by 12:00 NOON
+        EST on the following day except in circumstances where technical
+        failure, inability of the Company to verify your compliance with these
+        Terms, or other reasons prevent such timely payout. Contest prizes are
+        listed in our prize table list. Prizes won are added to the winning
+        participants account balance. In the event of a tie, prizes are divided
+        evenly amongst the participants that have tied.
+      </p>
+      <p className='f5 b'>PRIZE TABLES</p>
+      <p className='f5'>
+        Contest prize payouts will be published with the creation of each new
+        contest. Clouty reserves the right, in its sole discretion, to cancel or
+        suspend the contests (or any portion thereof) should virus, bugs,
+        unauthorized human intervention, or other causes corrupt administration,
+        security, fairness, integrity or proper operation of the contest (or any
+        portion thereof) warrant doing so. Notification of such changes may be
+        provided by Clouty to its customers but will not be required.
+      </p>
+      <p className='f5 b'>MISCELLANEOUS</p>
+      <p className='f5'>
+        These Terms of Use shall be governed by the internal substantive laws of
+        the Commonwealth of Delaware, without respect to its conflict of laws
+        principles. Any claim or dispute between you and Clouty that arises in
+        whole or in part from the Terms of Use, the Website or any Contest shall
+        be decided exclusively by a court of competent jurisdiction located in
+        Suffolk County, Delaware.
+      </p>{' '}
+      <p className='f5'>
+        Nothing in the Terms of Use shall create or confer any rights or other
+        benefits in favor of any third parties except as specifically provided
+        herein. By participating in any Contest on the Website, you agree to
+        indemnify, protect, defend and hold harmless Clouty, its parents,
+        subsidiaries, affiliates and divisions, and their respective directors,
+        officers, employees, agents and representatives (the &quot;Umbrella
+        Inc&quot;), from and against any and all third party claims,
+        liabilities, losses, damages, injuries, demands, actions, causes of
+        action, suits, proceedings, judgments and expenses, including reasonable
+        attorneys’ fees, court costs and other legal expenses including, without
+        limitation, those costs incurred at the trial and appellate levels and
+        in any bankruptcy, reorganization, insolvency or other similar
+        proceedings, and any other legal expenses (collectively,
+        &quot;Claims&quot;) arising from or connected with your use of the
+        Website, any payment methods used, any funding of your account, and/or
+        your participation in any Contest. The Website may contain links to
+        third party websites that are not owned or controlled by Clouty. Clouty
+        has no control over, and assumes no responsibility for, the content,
+        privacy policies, or practices of any third party websites. In addition,
+        Clouty will not and cannot censor or edit the content of any third-party
+        site. By using the Website, you expressly relieve Clouty from any and
+        all liability arising from your use of any third-party website.{' '}
+      </p>{' '}
+      <p className='f5'>
+        Accordingly, we encourage you to be aware when you leave the Website and
+        to read the terms and conditions and privacy policy of each other
+        website that you visit. Nothing in the Terms of Use shall create or be
+        deemed to create a partnership, agency, trust arrangement, fiduciary
+        relationship or joint venture between you and Clouty. No professional or
+        amateur sports league or any team associated with any professional or
+        amateur sports league is associated with Clouty or in any way affiliated
+        or associated with the Contests.
+      </p>{' '}
+      <p className='f5'>
+        Third-party online publishers that refer users to the Clouty website
+        shall not be responsible or liable for the Clouty website or any of the
+        content, software, or functions made available on, or accessed through,
+        or sent from, the Clouty website.
+      </p>{' '}
+      <p className='f5'>
+        If any provision of these Terms of Use is deemed invalid by a court of
+        competent jurisdiction, the invalidity of such provision shall not
+        affect the validity of the remaining provisions of these Terms of Use,
+        which shall remain in full force and effect.
+      </p>{' '}
+      <p className='f5'>
+        No waiver of any term of these Terms of Use shall be deemed a further or
+        continuing waiver of such term or any other term, and Clouty&apos;s
+        failure to assert any right or provision under these Terms of Use shall
+        not constitute a waiver of such right or provision.
+      </p>{' '}
+      <p className='f5'>
+        Clouty reserves the right to amend these Terms of Use at any time and
+        without notice, and it is your responsibility to review these Terms of
+        Use for any changes.
+      </p>{' '}
+      <p className='f5'>
+        If you continue to use the Services after we change the Terms of Use,
+        you accept all changes. The failure of Clouty to comply with any
+        provision of these Terms of Use due to an act of God, hurricane, war,
+        fire, riot, earthquake, terrorism, act of public enemies, actions of
+        governmental authorities outside of the control of the Company
+        (excepting compliance with applicable codes and regulations) or other
+        force majeure event will not be considered a breach of these Terms of
+        Use.
+      </p>{' '}
+      <p className='f5'>
+        CLOUTY AND OTHER TRADEMARKS CONTAINED ON THE WEBSITE ARE TRADEMARKS OR
+        REGISTERED TRADEMARKS OF CLOUTY IN THE UNITED STATES AND/OR OTHER
+        COUNTRIES. THIRD PARTY TRADEMARKS, TRADE NAMES, PRODUCT NAMES AND LOGOS
+        MAY BE THE TRADEMARKS OR REGISTERED TRADEMARKS OF THEIR RESPECTIVE
+        OWNERS. YOU MAY NOT REMOVE OR ALTER ANY TRADEMARK, TRADE NAMES, PRODUCT
+        NAMES, LOGO, COPYRIGHT OR OTHER PROPRIETARY NOTICES, LEGENDS, SYMBOLS OR
+        LABELS ON THE WEBSITE.
       </p>
     </Wrapper>
   </div>
