3f4c7fd6c0646c2a5d4033f7659ac4b0606fa816
ebrima <ebrima.jobe92@gmail.com>
2020-03-03 01:49:52 -0800

    get total wager, lost and won bets

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,3 +1,4 @@
+import { formatPrice } from '../../lib/helpers';
 import AdminDashboard from '../../components/AdminDashboard';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
@@ -7,7 +8,14 @@ import Wrapper from '../../components/Wrapper';
 import absoluteUrl from 'next-absolute-url';
 import axios from 'axios';
 
-const Dashboard = ({ entries, questions, users }) => {
+const Dashboard = ({
+  entries,
+  questions,
+  users,
+  totalWager,
+  lostBets,
+  wonBets,
+}) => {
   const data = {
     title: 'Dashboard',
     header: 'Clouty By The Numbers',
@@ -22,10 +30,12 @@ const Dashboard = ({ entries, questions, users }) => {
         </dl>
         <dl className='dib mr5'>
           <dd className='f6 f5-ns b ml0'>Wins/Losses</dd>
-          <dd className='f3 f2-ns b ml0'>23 - 38</dd>
+          <dd className='f3 f2-ns b ml0'>
+            {wonBets} - {lostBets}
+          </dd>
         </dl>
         <dl className='dib mr5'>
-          <dd className='f6 f5-ns b ml0'>Total Wagers</dd>
+          <dd className='f6 f5-ns b ml0'>Total Bets</dd>
           <dd className='f3 f2-ns b ml0'>{entries.length}</dd>
         </dl>
         <dl className='dib mr5'>
@@ -37,8 +47,8 @@ const Dashboard = ({ entries, questions, users }) => {
           <dd className='f3 f2-ns b ml0'>$37.00</dd>
         </dl>
         <dl className='dib mr5'>
-          <dd className='f6 f5-ns b ml0'>Total Payouts</dd>
-          <dd className='f3 f2-ns b ml0'>$37.00</dd>
+          <dd className='f6 f5-ns b ml0'>Total Wagers</dd>
+          <dd className='f3 f2-ns b ml0'>{formatPrice(totalWager)}</dd>
         </dl>
         <dl className='dib'>
           <dd className='f6 f5-ns b ml0'>Users</dd>
@@ -73,13 +83,34 @@ Dashboard.getInitialProps = async ({ req }) => {
   const usersRes = await axios.get(`${apiURL}/api/users`);
   const users = usersRes.data;
   const entries = res.data;
-  return { entries, questions, users };
+  console.log(
+    entries.reduce((acc, curr) => {
+      return acc + !curr.won ? 1 : 0;
+    }),
+  );
+  return {
+    entries,
+    questions,
+    users,
+    lostBets: entries.reduce((acc, curr) => {
+      return acc + !curr.won ? 1 : 0;
+    }),
+    wonBets: entries.reduce((acc, curr) => {
+      return acc + curr.won ? 1 : 0;
+    }),
+    totalWager: entries.reduce((acc, curr) =>
+      acc + curr.wager ? curr.wager : 0,
+    ),
+  };
 };
 
 Dashboard.propTypes = {
   entries: PropTypes.array,
   questions: PropTypes.array,
   users: PropTypes.array,
+  totalWager: PropTypes.number,
+  lostBets: PropTypes.number,
+  wonBets: PropTypes.number,
 };
 
 export default SecuredPage(Dashboard);
