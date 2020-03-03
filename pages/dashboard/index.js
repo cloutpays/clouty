cb95a19d0776f8142c575ece2add302bfa88a4fc
ebrima <ebrima.jobe92@gmail.com>
2020-03-02 23:58:47 -0800

    dashboard by the numbers

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -7,14 +7,40 @@ import Wrapper from '../../components/Wrapper';
 import absoluteUrl from 'next-absolute-url';
 import axios from 'axios';
 
-const Dashboard = ({ entries }) => {
+const Dashboard = ({ entries, questions }) => {
   const data = {
     title: 'Dashboard',
-    header: 'User Submissions',
+    header: 'Clouty By The Numbers',
     description: 'Dashboard',
   };
   return (
     <Wrapper data={data}>
+      <article className='pa2 pa2-ns' data-name='slab-stat'>
+        <dl className='dib mr5'>
+          <dd className='f6 f5-ns b ml0'>Total Games</dd>
+          <dd className='f3 f2-ns b ml0'>{questions.length}</dd>
+        </dl>
+        <dl className='dib mr5'>
+          <dd className='f6 f5-ns b ml0'>Wins/Losses</dd>
+          <dd className='f3 f2-ns b ml0'>23 - 38</dd>
+        </dl>
+        <dl className='dib mr5'>
+          <dd className='f6 f5-ns b ml0'>Total Wagers</dd>
+          <dd className='f3 f2-ns b ml0'>{entries.length}</dd>
+        </dl>
+        <dl className='dib mr5'>
+          <dd className='f6 f5-ns b ml0'>24-Hour Sales</dd>
+          <dd className='f3 f2-ns b ml0'>$14.00</dd>
+        </dl>
+        <dl className='dib mr5'>
+          <dd className='f6 f5-ns b ml0'>House Balance</dd>
+          <dd className='f3 f2-ns b ml0'>$37.00</dd>
+        </dl>
+        <dl className='dib'>
+          <dd className='f6 f5-ns b ml0'>Users</dd>
+          <dd className='f3 f2-ns b ml0'>4</dd>
+        </dl>
+      </article>
       <div className='flex'>
         <a
           href='/dashboard/create'
@@ -38,12 +64,15 @@ Dashboard.getInitialProps = async ({ req }) => {
   const { origin } = absoluteUrl(req);
   const apiURL = `${origin}`;
   const res = await axios.get(`${apiURL}/api/submissions`);
+  const questionsRes = await axios.get(`${apiURL}/api/questions`);
+  const questions = questionsRes.data;
   const entries = res.data;
-  return { entries };
+  return { entries, questions };
 };
 
 Dashboard.propTypes = {
   entries: PropTypes.array,
+  questions: PropTypes.array,
 };
 
 export default SecuredPage(Dashboard);
