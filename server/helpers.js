638547147f1fb81952c954a9fc3054307fb93bf0
ebrima <ebrima.jobe92@gmail.com>
2020-03-07 23:39:21 -0700

    now dev test

--- server/helpers.js
+++ server/helpers.js
@@ -1,7 +1,6 @@
 const connect = require('./helpers/db');
 
-export const dev =
-  process.env.NODE_ENV === 'development' || process.env.ENV === 'development';
+export const dev = process.env.NODE_ENV === 'development';
 export const question = dev ? 'questiondev' : 'question';
 export const cloutpays = dev ? 'cloutpaysdev' : 'cloutpays';
 export const user = dev ? 'userdev' : 'user';
