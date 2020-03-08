1b8c4e4bfe026cdb3290279f00043649379fbfe6
ebrima <ebrima.jobe92@gmail.com>
2020-03-07 23:09:25 -0700

    remove game play usability w/o acct + add reset button on bttom

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -1,8 +1,5 @@
 import axios from 'axios';
 import Router from 'next/router';
-
-import 'cleave.js/dist/addons/cleave-phone.us';
-import Cleave from 'cleave.js/react';
 import React, { useState } from 'react';
 import Question from './Question';
 interface SignUpFormProps {
@@ -11,15 +8,13 @@ interface SignUpFormProps {
   previousBet: any;
 }
 
-const SignUpForm: React.FC<SignUpFormProps> = ({ game, user, previousBet }) => {
-  const [name, setName] = useState<string>(user ? user.info.firstName : '');
-  const [email, setEmail] = useState<string>(user ? user.firebase.email : '');
-  const [handle, setHandle] = useState<string>(user ? user.info.firstName : '');
+const SignUpForm: React.FC<SignUpFormProps> = ({ game, user }) => {
+  const [name] = useState<string>(user ? user.info.firstName : '');
+  const [email] = useState<string>(user ? user.firebase.email : '');
+  const [handle] = useState<string>(user ? user.info.firstName : '');
   const [answer, setAnswer] = useState<string>('');
   const [options, setOptions] = useState<any>(game.options);
-  const [phoneNumber, setPhoneNumber] = useState<string>(
-    user ? user.info.phoneNumber : '',
-  );
+  const [phoneNumber] = useState<string>(user ? user.info.phoneNumber : '');
   const [wager, setWager] = useState<number[]>([1, 5, 10, 20, 50]);
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -60,129 +55,64 @@ const SignUpForm: React.FC<SignUpFormProps> = ({ game, user, previousBet }) => {
     <div className='row'>
       <div className='form-card'>
         <form onSubmit={handleSubmit}>
-          {!previousBet ? (
-            // {previousBet.length !== 0 ? (
-            <div>
-              <p className='b'>You have a hand in this bet.</p>
-            </div>
-          ) : (
-            <>
-              {!user && (
-                <>
-                  <div>
-                    <input
-                      className='game-input'
-                      type='text'
-                      value={name}
-                      placeholder='Name'
-                      onChange={(event) => setName(event.currentTarget.value)}
-                      name='name'
-                      required={true}
-                    />
-                  </div>
-                  <div>
-                    <input
-                      type='email'
-                      className='game-input'
-                      placeholder='Email address'
-                      value={email}
-                      onChange={(event) => setEmail(event.currentTarget.value)}
-                      name='email'
-                      required={true}
-                    />
-                  </div>
-
-                  <div>
-                    <input
-                      type='text'
-                      value={handle}
-                      className='game-input'
-                      placeholder='IG/Twitter Handle'
-                      onChange={(event) => setHandle(event.currentTarget.value)}
-                      name='handle'
-                      id='handle'
-                    />
-                  </div>
-                </>
-              )}
-              <div className='mt2'>
-                <label className='fw6' htmlFor='email'>
-                  Select your wager:
-                </label>
-                <br />
-                <div className='dib-ns'>
-                  <div className='flex flex-wrap'>
-                    {wager.map((wage, ind) => {
-                      return (
-                        <div
-                          onClick={() => setWager([Number(wage)])}
-                          key={ind}
-                          className='noselect grow outline dim pa2 mr2 mt2'>
-                          <strong>${wage}</strong>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
+          <div className='mt2'>
+            <label className='fw6' htmlFor='email'>
+              Select your wager:
+            </label>
+            <br />
+            <div className='dib-ns'>
+              <div className='flex flex-wrap'>
+                {wager.map((wage, ind) => {
+                  return (
+                    <div
+                      onClick={() => setWager([Number(wage)])}
+                      key={ind}
+                      className='noselect grow outline dim pa2 mr2 mt2'>
+                      <strong>${wage}</strong>
+                    </div>
+                  );
+                })}
               </div>
-              {!user && (
-                <>
-                  <div>
-                    <Cleave
-                      className='game-input'
-                      placeholder='Phone Number'
-                      onChange={(event) =>
-                        setPhoneNumber(event.currentTarget.value)
-                      }
-                      options={{ phone: true, phoneRegionCode: 'US' }}
-                    />
-                  </div>
-                </>
+            </div>
+          </div>
+
+          <Question
+            type={game.type}
+            options={options}
+            title={game.question}
+            setAnswer={confirmAnswer}
+          />
+          <br />
+          {options.length === 1 && wager.length === 1 && (
+            <div>
+              {wager[0] > user.stripe.user.balance / 100 ? (
+                <div className='mb2 f7 fw6 '>
+                  You have insufficient credits. Reload your balance.
+                </div>
+              ) : (
+                <div>
+                  <span
+                    onClick={resetGame}
+                    className='bg-white-30 pv1 pl2 pr3 mr2 f7 f6-ns br-pill b noselect'>
+                    <span className='pl1 sans-serif'>Reset</span>
+                  </span>
+                  <span
+                    onClick={handleSubmit}
+                    className='bg-white-30 pv1 pl2 pr3 f7 f6-ns br-pill b noselect'>
+                    <span className='pl1 sans-serif'>Play</span>
+                  </span>
+                </div>
               )}
-              <Question
-                type={game.type}
-                options={options}
-                title={game.question}
-                setAnswer={confirmAnswer}
-              />
-              <br />
-              {phoneNumber &&
-                options.length === 1 &&
-                wager.length === 1 &&
-                name &&
-                email && (
-                  <div>
-                    {wager[0] > user.stripe.user.balance / 100 ? (
-                      <div className='mb2 f7 fw6 '>
-                        You have insufficient credits. Reload your balance.
-                      </div>
-                    ) : (
-                      <div>
-                        <span
-                          onClick={resetGame}
-                          className='bg-white-30 pv1 mr2 pl2 pr3 f7 f6-ns br-pill b noselect'>
-                          <span className='pl1 sans-serif'>Reset</span>
-                        </span>
-                        <span
-                          onClick={handleSubmit}
-                          className='bg-white-30 pv1 pl2 pr3 f7 f6-ns br-pill b noselect'>
-                          <span className='pl1 sans-serif'>Play</span>
-                        </span>
-                      </div>
-                    )}
-                  </div>
-                )}
-              {options.length !== 1 ||
-                (wager.length !== 1 && (
-                  <div>
-                    <span
-                      onClick={resetGame}
-                      className='bg-white-30 pv1 pl2 pr3 f7 f6-ns br-pill b noselect'>
-                      <span className='pl1 sans-serif'>Reset</span>
-                    </span>
-                  </div>
-                ))}
-            </>
+            </div>
+          )}
+          {options.length + wager.length > 2 && (
+            <div>
+              <span
+                onClick={resetGame}
+                className='bg-white-30 pv1 pl2 pr3 f7 f6-ns br-pill b noselect'>
+                <span className='pl1 sans-serif'>Reset</span>
+              </span>
+            </div>
           )}
         </form>
       </div>
