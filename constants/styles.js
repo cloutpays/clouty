fb2e1990617cf4536d00bb8d0fd18214e71c116c
ebrima <ebrima.jobe92@gmail.com>
2020-03-15 13:05:58 -0700

    paragraph and link styles

--- constants/styles.js
+++ constants/styles.js
@@ -1,10 +1,10 @@
 const styles = {
   wrapper: 'mw8 center',
-  link: 'near-black fw7 link no-underline bb bw1 b--green dim',
+  link: 'gray fw7 link no-underline bb bw1 dim',
   linkNormalWeight: 'near-black link no-underline bb bw1 b--orange dim',
   navigationLink: 'near-black fw7 link no-underline',
   unorderedList: 'list unordered-list lh-copy',
-  paragraph: 'lh-copy',
+  paragraph: 'lh-copy tc gray',
   h1: 'f3 f2-ns',
   h2: 'f4 f3-ns mt4 mt5-ns',
   h3: 'f5 f4-ns mt3 mt4-ns mb0',
