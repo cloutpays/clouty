fc8e72ba1767fe05155b365bcc58dfdac9392a74
Ebrima <ebrima.jobe92@gmail.com>
2020-03-15 22:42:16 -0700

    xxl card preview

--- pages/games/index.js
+++ pages/games/index.js
@@ -31,7 +31,27 @@ const Games = ({ questions }) => {
             </div>
           </a>
         </div>
-
+        <div key='contact' className='pv2 pa2-ns w-100 w-50-ns'>
+          <div className='white br2 shadow-4 grow pa3 pa4-ns h-100 xxl'>
+            <h1 className='f4 mt0 fw7'>XXL Freshman Class</h1>
+            <p>
+              Don&apos;t miss your chance to bet on the upcoming Freshman Class
+              of 2020!
+            </p>
+            <p>Click to enter and place your wagers </p>
+            <span className='bg-white-30 pv1 ph2 f7 f6-ns br-pill b'>
+              Play Game
+              <span className='pl1 sans-serif'>→</span>
+            </span>
+            <span className='pv1 ph2 f7 fr f6-ns br-pill b'>
+              <img
+                width='35px'
+                height='23px'
+                src='https://upload.wikimedia.org/wikipedia/commons/2/24/XXL_magazine.png'
+              />
+            </span>
+          </div>
+        </div>
         {questions
           .reverse()
           .sort((a, b) => {
