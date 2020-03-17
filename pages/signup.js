03c2297ce1c1c9101c1da4e2cb5a3bf7b0ca7349
ebrima <ebrima.jobe92@gmail.com>
2020-03-16 20:23:59 -0700

    sign up updates

--- pages/signup.js
+++ pages/signup.js
@@ -1,14 +1,18 @@
+import { styles } from '../constants/styles';
 import LoginForm from '../components/LoginForm';
 import React from 'react';
 import Wrapper from '../components/Wrapper';
 
 const data = {
   description: 'Make money while putting your intuition on the line.',
-  header: `The 🌎's first music betting platform.`,
+  header: `Start Playing Clouty`,
 };
 const Login = () => (
   <div>
     <Wrapper data={data}>
+      <p className={styles.paragraph}>
+        Sign up today to receive a free $5 towards your bets.
+      </p>
       <LoginForm mode={'signup'} />
     </Wrapper>
   </div>
