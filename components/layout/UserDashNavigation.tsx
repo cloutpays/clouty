63a555f1773ca421c5d058e7d8aec46326ce6004
ebrima <ebrima.jobe92@gmail.com>
2020-03-22 02:09:13 -0700

    user dash styling

--- components/layout/UserDashNavigation.tsx
+++ components/layout/UserDashNavigation.tsx
@@ -30,8 +30,8 @@ const Navigation: React.FC<NavigationProps> = ({ balance, user }) => {
   }, []);
   return (
     <>
-      <nav className='w-100 mb0-l ph3-m ph3-l flex center'>
-        <div className='mr5'>
+      <article className='w-100 mb0-l ph3-m ph3-l flex center'>
+        <div className='mr5 pr3'>
           <h2 className='ttu mt0 mb2 f6 fw5 silver'>Manage</h2>
           <ul className='list pl0 mt0 mb4'>
             <li className='mb2'>
@@ -71,29 +71,26 @@ const Navigation: React.FC<NavigationProps> = ({ balance, user }) => {
             </li>
           </ul>
         </div>
-      </nav>
+      </article>
       <article className='w-100 mb0-l ph3-m ph3-l flex center'>
-        <div className='mr5'>
-          <header className='mb3'>
-            <h2 className='ttu mt0 mb1 f6 fw5 silver'>Balance</h2>
-            <h1 className='fw3 dark-gray mt0 mb0'>
-              {isLoading ? (
-                <i className='fa fa-spinner fa-spin' />
-              ) : (
-                formatPrice(userBalance / 100)
-              )}
-            </h1>
-            <Link href='/user/balance'>
-              <a className=' f6 no-underline fw5 mt3 br2 ph3 pv2 dib ba b--blue blue bg-white hover-bg-blue hover-white'>
-                Add to Balance
-              </a>
-            </Link>
-          </header>
+        <div className='mr5 pr4 pr5-ns'>
+          <h2 className='ttu mt0 mb1 f6 fw5 silver'>Balance</h2>
+          <h1 className='fw3 dark-gray mt0 mb0'>
+            {isLoading ? (
+              <i className='fa fa-spinner fa-spin' />
+            ) : (
+              formatPrice(userBalance / 100)
+            )}
+          </h1>
+          <Link href='/user/balance'>
+            <a className=' f6 no-underline fw5 mt3 br2 ph3 pv2 dib ba b--blue blue bg-white hover-bg-blue hover-white'>
+              Add to Balance
+            </a>
+          </Link>
         </div>
         {user.stripe.user.credit > 0 && (
           <div>
-            {' '}
-            <h2 className='ttu mt0 mb1 f6 fw5 silver'>Credit</h2>
+            <h2 className='ttu mt0 mb2 f6 fw5 silver'>Credit</h2>
             <h1 className='fw3 dark-gray mt0 mb0'>
               {isLoading ? (
                 <i className='fa fa-spinner fa-spin' />
