14e52fb38f7d7e2fd0310dffbc635672803d935c
Julius Buckley <julius@goodtime.io>
2020-03-22 13:07:06 -0700

    change balance to 200

--- server/helpers/user.js
+++ server/helpers/user.js
@@ -48,7 +48,7 @@ export const userApi = wrapAsync(async function(req, db) {
     });
 
     userData.stripe = {
-      user: { ...stripeUser, balance: 500 },
+      user: { ...stripeUser, balance: 200 },
     };
     userData.new = false;
   }
