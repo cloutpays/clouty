c9e04b8034be7c7582306ff58c68173cedf8c154
Ebrima <ebrima.jobe92@gmail.com>
2020-03-28 09:30:13 -0700

    point to prod

--- server/helpers.js
+++ server/helpers.js
@@ -5,7 +5,8 @@ const client = require('twilio')(
   process.env.TWILIO_SID,
   process.env.TWILIO_TOKEN,
 );
-const dev = true;
+const dev =
+  process.env.ENV === 'development' || process.env.NODE_ENV === 'development';
 
 const staging = process.env.ENV === 'staging';
 export const question = dev
