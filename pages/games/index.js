42277196d49653d98e22f0ea5c1b88fe26cb6089
Julius Buckley <juliusbuckley@gmail.com>
2020-04-05 21:59:17 -0700

    wrap in Link

--- pages/games/index.js
+++ pages/games/index.js
@@ -16,21 +16,23 @@ const Games = ({ questions }) => {
     <Wrapper data={data}>
       <section className='ma3 ma4-l flex flex-wrap'>
         <div key='createGame' className='pv2 pa2-ns w-100 w-50-ns'>
-          <a href='/games/create' className='no-underline'>
-            <div className='white br2 shadow-4 grow pa3 pa4-ns h-100 contact-card'>
-              <h1 className='f4 mt0 fw7'>
-                <span role='img' aria-label='User Submission'>
-                  🗳
-                </span>{' '}
-                Want to create a game?
-              </h1>
-              <p>If it&apos;s good, we will add it to our weekly games.</p>
-              <p>Sound good?</p>
-              <span className='bg-white-30 pv1 ph2 f7 f6-ns br-pill b'>
-                Create Game<span className='pl1 sans-serif'>→</span>
-              </span>
-            </div>
-          </a>
+          <Link href='/games/create'>
+            <a className='no-underline'>
+              <div className='white br2 shadow-4 grow pa3 pa4-ns h-100 contact-card'>
+                <h1 className='f4 mt0 fw7'>
+                  <span role='img' aria-label='User Submission'>
+                    🗳
+                  </span>{' '}
+                  Want to create a game?
+                </h1>
+                <p>If it&apos;s good, we will add it to our weekly games.</p>
+                <p>Sound good?</p>
+                <span className='bg-white-30 pv1 ph2 f7 f6-ns br-pill b'>
+                  Create Game<span className='pl1 sans-serif'>→</span>
+                </span>
+              </div>
+            </a>
+          </Link>
         </div>
         <div key='contact' className='pv2 pa2-ns w-100 w-50-ns'>
           <Link href='https://clouty.substack.com/' prefetch={false}>
