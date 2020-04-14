40c94140be4c9d2a4496b3943ea4fe120ad4209c
ebrima <ebrima.jobe92@gmail.com>
2020-04-13 23:53:54 -0700

    reset staging

--- server/helpers.js
+++ server/helpers.js
@@ -8,9 +8,7 @@ const client = require('twilio')(
 export const dev =
   process.env.ENV === 'development' || process.env.NODE_ENV === 'development';
 
-export const staging = true;
-// export const staging = (req) =>
-// req.headers.origin !== 'clouty.io' || req.headers.origin !== 'getclouty.com';
+export const staging = process.env.ENV === 'staging';
 
 export const question = dev
   ? 'question_dev'
