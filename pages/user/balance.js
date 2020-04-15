389648b38e57b5f5acae0ae6f6e2f3bfaeaced75
ebrima <ebrima.jobe92@gmail.com>
2020-04-15 00:10:38 -0700

    user dash ui update

--- pages/user/balance.js
+++ pages/user/balance.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import PropTypes from 'prop-types';
 import React from 'react';
 import SecuredPage from '../../hoc/securedPage';
+import UserDashNavigation from '../../components/layout/UserDashNavigation';
 import Wrapper from '../../components/layout/Wrapper';
 import absoluteUrl from 'next-absolute-url';
 import axios from 'axios';
@@ -21,8 +22,13 @@ const Terms = (ctx) => {
       </Head>
       <Wrapper data={data} className='measure-wide'>
         <main className='black-80'>
-          {/* <UserDashNavigation /> */}
-          <div className='mw8 center pv3 ph3' id='dashboard'>
+          <UserDashNavigation user={user} />
+          <div className='mw8 center pb2 ph3' id='dashboard'>
+            <article className='pl3-ns mt2 pt1'>
+              <div className='mr5 pr2 pr5-ns'>
+                <h2 className='ttu mt0 mb1 f6 fw5 silver'>Select Amount</h2>
+              </div>
+            </article>
             <section className='flex-m flex-l nl3-m nr3-m nl3-l nr3-l'>
               <article className='w-100 w-75-l ph3-m ph3-l'>
                 <CardSection user={user} />
