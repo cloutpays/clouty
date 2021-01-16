f05a54d0926f163c11ddacabe85bf334c62cb222
ebrima <ebrima.jobe92@gmail.com>
2021-01-15 19:45:42 -0800

    update about segments

--- pages/about.js
+++ pages/about.js
@@ -7,41 +7,52 @@ const data = {
 };
 const Home = () => (
   <Wrapper data={data}>
-    <div className='about-page' style={{backgroundColor: "#EFE8F4", marginTop: "-8vw"}}>
+    <div
+      className='about-page'
+      style={{ backgroundColor: '#EFE8F4', marginTop: '-8vw' }}>
       <div className='row'>
         <div className='col-md-12'>
-          <div className="d-flex-about">
-          <div className='about-banner-content'>
-            <div className='row'>
-              <div className='col-md-6'>
-                <p>Our Story</p>
-                <h1>We're the first <br></br>  of our kind. 🥇</h1>
+          <div className='d-flex-about'>
+            <div className='about-banner-content'>
+              <div className='row'>
+                <div className='col-md-6'>
+                  <p>Our Story</p>
+                  <h1>
+                    We’re the first <br></br> of our kind. 🥇
+                  </h1>
+                </div>
               </div>
+              <div className='row'>
+                <div className='col-md-6'>
+                  <p>
+                    Clouty was born December 2019 when our founder, David Umeh
+                    discovered the artists Foggieraw and Ankhlejohn. Upon
+                    hearing their cadences and presence he knew they had serious
+                    shot at making it big. In a large group-chat where nothing
+                    but music was discussed and debated, he went on to say,
+                    &ldquo;
+                    <strong>
+                      This guy is gonna blow....if I could put my money on it, I
+                      would.
+                    </strong>
+                    &ldquo;
+                  </p>
+                </div>
+                <div className='col-md-6'>
+                  <p>
+                    Upon hearing their cadences and presence he knew they had
+                    serious shot at making it big. In a large group-chat where
+                    nothing but music was discussed and debated, he went on to
+                    say,{' '}
+                    <strong>
+                      The first and only platform when audiophiles could put
+                      their money where their ears are.
+                    </strong>
+                  </p>
+                </div>
+              </div>
+              <button className='btn btn-default'>Contact Us</button>
             </div>
-            <div className='row'>
-              <div className='col-md-6'>
-                <p>
-                  Clouty was born December 2019 when our founder, David Umeh
-                  discovered the artists Foggieraw and Ankhlejohn. Upon hearing
-                  their cadences and presence he knew they had serious shot at
-                  making it big. In a large group-chat where nothing but music
-                  was discussed and debated, he went on to say,
-                  &ldquo;<strong>This guy is gonna blow....if I could put my money on it, I
-                  would.</strong>&ldquo;
-                </p>
-              </div>
-              <div className='col-md-6'>
-                <p>
-                  Upon hearing their cadences and presence he knew they had
-                  serious shot at making it big. In a large group-chat where
-                  nothing but music was discussed and debated, he went on to
-                  say, <strong>The first and only platform when audiophiles could put
-                  their money where their ears are.</strong>
-                </p>
-              </div>
-            </div>
-            <button className='btn btn-default'>Contact Us</button>
-          </div>
           </div>
         </div>
       </div>
@@ -50,18 +61,30 @@ const Home = () => (
       <div className='about-container'>
         <div className='row'>
           <div className='col-md-3 title'>
-            <h1>Our Mission <br></br> At Clouty</h1>
+            <h1>
+              Our Mission <br></br> At Clouty
+            </h1>
           </div>
           <div className='col-md-9'>
             <div className='row'>
               <div className='col-md-6'>
                 <div className='about-service-item'>
-                  <h5>Clarity</h5>
+                  <h5>Reimagine</h5>
                   <p>
-                    Creative conversion buzz seed money twitter alpha release
-                    analytics termsheet MVP early adopters long tail
-                    business-to-consumer strategy. Series A financing paradigm
-                    shift growth hacking bootstrapping seed money.
+                    We’re allowing people to “predict the weather” and bet on
+                    predictions on the forecast of their favorite artists backed
+                    by data in the music industry, starting with rap.
+                  </p>
+                </div>
+              </div>
+              <div className='col-md-6'>
+                <div className='about-service-item'>
+                  <h5>Discovery</h5>
+                  <p>
+                    As the platform grows and evolves, our vision for the future
+                    includes a realm where clout is not a pre requisite but
+                    instead a side effect of creativity and pure artistry. It’s
+                    time to level the playing field.
                   </p>
                 </div>
               </div>
@@ -69,32 +92,19 @@ const Home = () => (
                 <div className='about-service-item'>
                   <h5>Simplicity</h5>
                   <p>
-                    Creative conversion buzz seed money twitter alpha release
-                    analytics termsheet MVP early adopters long tail
-                    business-to-consumer strategy. Series A financing paradigm
-                    shift growth hacking bootstrapping seed money.
+                    We are aware that this is a new industry in of itself. At no
+                    point in this process do we want anyone to find themselves
+                    confused. We encourage reaching out to our help line and
+                    will continue to implement features that embody ease of use.
                   </p>
                 </div>
               </div>
               <div className='col-md-6'>
                 <div className='about-service-item'>
-                  <h5>Clarity</h5>
+                  <h5>Music</h5>
                   <p>
-                    Creative conversion buzz seed money twitter alpha release
-                    analytics termsheet MVP early adopters long tail
-                    business-to-consumer strategy. Series A financing paradigm
-                    shift growth hacking bootstrapping seed money.
-                  </p>
-                </div>
-              </div>
-              <div className='col-md-6'>
-                <div className='about-service-item'>
-                  <h5>Simplicity</h5>
-                  <p>
-                    Creative conversion buzz seed money twitter alpha release
-                    analytics termsheet MVP early adopters long tail
-                    business-to-consumer strategy. Series A financing paradigm
-                    shift growth hacking bootstrapping seed money.
+                    Almost needless to say, we at Clouty are music lovers and
+                    that is the foundation for everything Clouty represents.
                   </p>
                 </div>
               </div>
@@ -112,10 +122,10 @@ const Home = () => (
               <h1>Ready to get started?</h1>
               <div>
                 <span>
-                  You're just minutes away from placing your first ever music
+                  You’re just minutes away from placing your first ever music
                   bet.
                   <br />
-                  We'll even give you $2 free when you sign up!
+                  We’ll even give you $2 free when you sign up!
                 </span>
               </div>
               <button className='btn btn-default'>Sign Up</button>
